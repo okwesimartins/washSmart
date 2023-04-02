f14805aec3e3f6949ca6d0a7736d43283135a73a
macbook <macbook@macbooks-MacBook-Pro.local>
2023-04-02 12:27:01 +0100

    new

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ import router from "./routes/user-routes.js";
 import pricingRouter  from "./routes/pricing-routes.js";
 import bookingRouter from "./routes/booking-routes.js";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const app = express();
 
+
+// var corsOptions = {
+//     origin: 'http://localhost:8080',
+//     optionsSuccessStatus: 200 // For legacy browser support
+// }
+
+// app.use(cors(corsOptions));
 
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use("/api/user", router);
 app.use("/api/pricing", pricingRouter);
@@ -21,3 +30,5 @@ app.use("/api/booking", bookingRouter);
 mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI).then(()=> app.listen(5000)).then(()=> console.log("connected")
 ).catch((err)=> console.log(err));
+
+
